e269fe8fc0bf3a48210a32c263617eda1995b817
noor nayem <nayem16194@gmail.com>
2025-03-13 03:46:25 +0000

    Respawn & Button Texture Changes

--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -7,22 +7,25 @@ public class PlayerMotor : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool isGrounded;
-    public float speed =5f;
+    public float speed = 5f;
     public float gravity = -9.8f;
     public float jumpHeight = 5f;
-// hi
-    
-    // Start is called before the first frame update
+
+    private Vector3 startPosition;
+    private Vector3 currentCheckpointPosition; // Store checkpoint position
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        startPosition = transform.position;
+        currentCheckpointPosition = startPosition; // Initialize to start
     }
 
-    // Update is called once per frame
     void Update()
     {
         isGrounded = controller.isGrounded;
     }
+
     public void ProcessMove(Vector2 input)
     {
         Vector3 moveDirection = Vector3.zero;
@@ -35,13 +38,30 @@ public class PlayerMotor : MonoBehaviour
             playerVelocity.y = -2f;
         }
         controller.Move(playerVelocity * Time.deltaTime);
-        //Debug.Log(playerVelocity.y);
     }
+
     public void Jump()
     {
         if (isGrounded)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.5f * gravity);
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("TeleportFloor"))
+        {
+            transform.position = currentCheckpointPosition; // Teleport to checkpoint
+            playerVelocity.y = 0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            currentCheckpointPosition = other.transform.position; // Update checkpoint
         }
     }
 }
